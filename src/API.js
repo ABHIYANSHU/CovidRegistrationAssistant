d52734358a38023e013c096640999263ddc0b262
Abhinav Srivastava <abhiyanshu11@gmail.com>
2021-05-23 21:46:30 +0530

    V1

--- src/API.js
+++ src/API.js
@@ -1,17 +1,53 @@
 import axios from 'axios'
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc } from "firebase/firestore"
 
 const api = 
 {
-    getData: () => 
+    getStates: () => 
     {    
-    return axios({
+        return axios({
             'method':'GET',
             'url':'https://cdn-api.co-vin.in/api/v2/admin/location/states',
             'headers': {
-                'accept':'application/json',
-                'Accept-Language':'en_US'
+                'accept':'application/json'
             }
         })
+    },
+
+    getDistrict: (stateid) =>
+    {
+        return axios({
+            'method':'GET',
+            'url':'https://cdn-api.co-vin.in/api/v2/admin/location/districts/' + stateid,
+            'headers': {
+                'accept':'application/json'
+            }
+        })
+    },
+
+    storeData: async(contact, pincode, districtid) =>
+    {
+        initializeApp({
+            apiKey: 'xxxxxxxxxxx',
+            authDomain: 'xxxxxxxxxxx.firebaseapp.com',
+            projectId: 'xxxxxxxxxxxxxxxxx'
+          });
+        
+        const db = getFirestore();
+
+        try {
+            const docRef = await addDoc(collection(db, "xxxxxxxxx"), {
+              contactno: contact,
+              pincode: pincode,
+              district: districtid,
+              sended: 0
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
     }
 }
 
--- src/App.js
+++ src/App.js
@@ -1,41 +1,117 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from './API';
 
 function App() {
 
-  const [responseData, setResponseData] = useState('')
+  const [contact, setContact] = useState("")
+  const [opt, selectOption] = useState("F") 
+  const [states, setStates] = useState([])
+  const [selectedState, setSelectedState] = useState("")
+  const [districts, setDistricts] = useState([])
+  const [selectedDistrict, setSelectedDistrict] = useState("")
+  const [pincode, selectedPincode] = useState("")
+  const [IsSuccessful, setSuccess] = useState(false)
 
-  const fetchData = (e) => {
-    e.preventDefault()
-    api.getData()
-    .then((response)=>{
-        setResponseData(response.data)
-        console.log(response)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-}
+  useEffect(() => {
+    api.getStates()
+       .then((response)=>{
+         const listOfstates = response.data.states;
+         console.log(listOfstates)
+         setStates(listOfstates)
+       })
+       .catch((error) => {
+         console.log(error)
+       })
+    setSelectedState("1")
+  }, [])
 
-
+  useEffect(() => {
+    api.getDistrict(selectedState)
+       .then((response) => {
+          const listOfdistricts = response.data.districts;
+          console.log(listOfdistricts)
+          setDistricts(listOfdistricts)
+       })
+       .catch((error) => {
+         console.log(error)
+       })
+  }, [selectedState])
+  
   return (
-    <>
-      <p> Contact No. : </p><input type = "text" />
-      <br/>
-      <p> Pin Code : </p><input type="text" />
-      <br/>
-      <p> State : </p>
-      <select>
-        <option>UP</option>
-      </select>
-      <p>District : </p>
-      <select>
-        <option>Varanasi</option>
-      </select>
-      <button onClick = { fetchData } >States</button>
-      { responseData }
-    </>
+    <div className="container">
+      <div className="header">
+        <h1>Covid Vaccine Registration Assistant</h1>
+      </div>
+      {
+        IsSuccessful ?
+        <>
+          <h3 className = "message">You will be notified when slots are available </h3>
+        </>
+        :
+        <>
+          <input className = "whatsapp" type = "text" placeholder="Whatsapp Number" onChange = { (e) => { setContact(e.target.value) } } />
+          <div className="options" onChange = { (e) => { selectOption(e.target.value) } } >
+            <input type="radio" name="options" value = "T" /><p> Pincode </p>
+            &nbsp;&nbsp;&nbsp;
+            <input type="radio" name="options" value = "F" /><p> District </p>
+          </div>
+          {
+          opt==="T" ?
+          <div className="pincode">
+            <input type="text" placeholder="Pincode" onChange = { (e) => { selectedPincode(e.target.value) } } />
+          </div>
+          :
+          <>
+            <div className="state">
+              <p>State:</p>
+              &nbsp;&nbsp;
+              <select value = { selectedState } onChange = { (e) => { setSelectedState(e.target.value) } } >
+              {
+                states.map((item) => {
+                  return <option key = { item.state_id } value = { item.state_id } > { item.state_name } </option>
+                })
+              }
+              </select>
+            </div>
+            <div className="district">
+              <p>District:</p>
+              &nbsp;&nbsp;
+              <select value = { selectedDistrict } onChange = { (e) => { setSelectedDistrict(e.target.value) } } >
+              {
+                districts.map((item) => {
+                  return <option key = { item.district_id } value = { item.district_id } > { item.district_name } </option>
+                })
+              }
+              </select>
+            </div>
+          </>
+          }
+          {
+          contact.length === 10 && ( (opt === "T" && pincode.length === 6) || (opt === "F") ) ?
+          <button onClick = { () => {
+            api.storeData(contact, pincode, selectedDistrict)
+            setSuccess(!IsSuccessful);
+          } }
+          className = "btn"
+          > Submit </button>
+          :
+          <button className = "btn" disabled >Submit</button>
+          }
+          <div className="steps">
+            <ul>
+              <li>Enter your whatsapp number, on which you want to recieve notifications.</li>
+              <li>Either enter your pincode or select your state and district</li>
+              <li>Click Submit</li>
+              <li>You will recieve a notification message on your provided Whatsapp number</li>
+            </ul>
+          </div>
+        </>
+      }
+      <div className="footer">
+        <h3>Developed By - Abhinav Srivastava</h3>
+      </div>
+    </div>
   );
 }
 
